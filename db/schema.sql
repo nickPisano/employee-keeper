6da1b3ab514cdf80d5e7592e229bc49deb096d51
Nick Pisano <pisano.nicholas@yahoo.com>
2022-07-22 02:37:22 -0700

    Finished schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,15 @@ USE employees_db;
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
-)
+);
+
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -15,10 +23,3 @@ CREATE TABLE employee (
   role_id INT NOT NULL,
   manager_id INT 
 );
-
-CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
-)
